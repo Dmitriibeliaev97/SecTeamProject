8cbfeeccd0fd8dde7ad3ec9fa96825bcf245417e
Dmitrii Beliaev <dmitriymay97@gmail.com>
2023-10-05 19:21:38 +0300

    Добавил логи от библиотеки Ломбок

--- src/main/java/ru/secteam/teamwork/TeamworkApplication.java
+++ src/main/java/ru/secteam/teamwork/TeamworkApplication.java
@@ -1,16 +1,14 @@
 package ru.secteam.teamwork;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@Slf4j
 @SpringBootApplication
 public class TeamworkApplication {
-	private static final Logger logger = LoggerFactory.getLogger(TeamworkApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(TeamworkApplication.class, args);
-		logger.info("Бот запущен");
+		log.info("Бот запущен");
 	}
 
 }
