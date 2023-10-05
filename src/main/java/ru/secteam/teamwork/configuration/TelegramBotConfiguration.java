668fc7dbbf037e6996bf22276c0eab449fa14415
e-lysanov <e.lysanov@yandex.ru>
2023-10-05 17:52:02 +0300

    6th commit: 1. смержил изменения с изменениями Димы

--- src/main/java/ru/secteam/teamwork/configuration/TelegramBotConfiguration.java
+++ src/main/java/ru/secteam/teamwork/configuration/TelegramBotConfiguration.java
@@ -1,18 +1,21 @@
 package ru.secteam.teamwork.configuration;
 
-import lombok.*;
+import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.model.DeleteMyCommands;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 
-@Data
 @Configuration
-@PropertySource("application.properties")
 public class TelegramBotConfiguration {
-    @Value("${telegram.bot.name}")
-    String botName;
 
     @Value("${telegram.bot.token}")
-    String token;
+    private String token;
 
+    @Bean
+    public TelegramBot telegramBot() {
+        TelegramBot bot = new TelegramBot(token);
+        bot.execute(new DeleteMyCommands());
+        return bot;
+    }
 }
