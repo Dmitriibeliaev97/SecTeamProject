99b25e5ec879ad6e00e192bb0cacc1c244d8b845
Dmitrii Beliaev <dmitriymay97@gmail.com>
2023-10-16 10:35:11 +0300

    Добавлена свагер документация к VolunteerController

--- src/main/java/ru/secteam/teamwork/controllers/VolunteerController.java
+++ src/main/java/ru/secteam/teamwork/controllers/VolunteerController.java
@@ -1,8 +1,15 @@
 package ru.secteam.teamwork.controllers;
 
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import ru.secteam.teamwork.model.Animal;
 import ru.secteam.teamwork.model.Volunteer;
 import ru.secteam.teamwork.services.VolunteerService;
 
@@ -24,8 +31,18 @@ public class VolunteerController {
      * @param volunteer
      * @return
      */
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Добавление нового волонтера",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = Volunteer.class)
+                    )
+
+            )
+    })
     @PostMapping
-    public Volunteer add(@RequestBody Volunteer volunteer) {
+    public Volunteer add(@Parameter(description = "Все параметры добавляемого волонтера") @RequestBody Volunteer volunteer) {
         return volunteerService.add(volunteer);
     }
 
@@ -34,8 +51,18 @@ public class VolunteerController {
      * @param chatId
      * @return
      */
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Получение информации о волонтере по ID",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = Volunteer.class)
+                    )
+
+            )
+    })
     @GetMapping("/{chatId}")
-    public ResponseEntity<Volunteer> get(@PathVariable Long chatId) {
+    public ResponseEntity<Volunteer> get(@Parameter(description = "ID приюта") @PathVariable Long chatId) {
         Volunteer volunteer = volunteerService.get(chatId);
         if (volunteer == null) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
@@ -49,8 +76,19 @@ public class VolunteerController {
      * @param volunteer
      * @return
      */
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Изменение данных о приюте",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = Volunteer.class)
+                    )
+
+            )
+    })
     @PutMapping("/update/{chatId}")
-    public ResponseEntity<Volunteer> update(@PathVariable Long chatId, @RequestBody Volunteer volunteer) {
+    public ResponseEntity<Volunteer> update(@Parameter(description = "ID изменяемого приюта") @PathVariable Long chatId,
+                                            @Parameter(description = "Новые параметры приюта для замены") @RequestBody Volunteer volunteer) {
         Volunteer savedVolunteer = volunteerService.update(chatId, volunteer);
         if (savedVolunteer == null) {
             return ResponseEntity.badRequest().build();
@@ -64,8 +102,18 @@ public class VolunteerController {
      * @param chatId
      * @return
      */
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Удаление приюта",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = Volunteer.class)
+                    )
+
+            )
+    })
     @DeleteMapping("/{chatId}")
-    public ResponseEntity delete(@PathVariable Long chatId) {
+    public ResponseEntity delete(@Parameter(description = "ID удаляемого приюта") @PathVariable Long chatId) {
         volunteerService.delete(chatId);
         return ResponseEntity.ok().build();
     }
